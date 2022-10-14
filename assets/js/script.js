fdac4fcbf38fe59b49cddc4ce99d705e7be829ac
orianehnr <orianehnr@gmail.com>
2022-10-14 21:23:17 +0200

    Ajouts de fichiers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,4 @@
-/*function changeBackgroundColor() {
-    if (window.scrollY > window.innerHeight / 2) {
-        document.body.classList.add('bg-color')
-    } else {
-        document.body.classList.remove('bg-color')
-    }
-}
 
-window.addEventListener('scroll', changeBackgroundColor);*/
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > window.innerHeight / 2) {
-        document.body.classList.add('bg-color')
-    } else {
-        document.body.classList.remove('bg-color')
-    }
-});
 
 
 
